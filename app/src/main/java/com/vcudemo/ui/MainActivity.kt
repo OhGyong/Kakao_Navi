35a8a24eead8fa9f229178dc02d79fd9686e01ee
O_Gyong <gyoh@wizardlab.co.kr>
2023-09-07 15:41:02 +0900

    임시 NavigationActivity 이동

--- app/src/main/java/com/vcudemo/ui/MainActivity.kt
+++ app/src/main/java/com/vcudemo/ui/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.vcudemo.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.vcudemo.R
+import com.vcudemo.ui.map.NavigationActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_navi).setOnClickListener {
+            val intent = Intent(this, NavigationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/vcudemo/ui/map/NavigationActivity.kt
@@ -0,0 +1,7 @@
+package com.vcudemo.ui.map
+
+import com.vcudemo.base.BaseActivity
+
+class NavigationActivity : BaseActivity() {
+
+}
