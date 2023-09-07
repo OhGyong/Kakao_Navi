f9f6ba5a9a49959c616b16ff1af38f0baa983c6f
O_Gyong <gyoh@wizardlab.co.kr>
2023-09-07 14:35:54 +0900

    MainActivity 패키지 위치 이동

--- app/src/main/java/com/vcudemo/ui/MainActivity.kt
+++ app/src/main/java/com/vcudemo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vcudemo
+package com.vcudemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vcudemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
