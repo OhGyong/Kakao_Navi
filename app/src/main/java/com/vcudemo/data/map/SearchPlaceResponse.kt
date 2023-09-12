5e2239216e93f23989375d5f88cf9e844e93bce4
O_Gyong <ogyong7143@gmail.com>
2023-09-12 13:14:05 +0900

    SearchPlaceResponse 응답 SerializedName 수정

--- app/src/main/java/com/vcudemo/data/map/SearchPlaceResponse.kt
+++ app/src/main/java/com/vcudemo/data/map/SearchPlaceResponse.kt
@@ -7,30 +7,30 @@ data class SearchPlaceResponse(
     @SerializedName("documents") val documents: List<Document>
 ) {
     data class Meta(
-        @SerializedName("totalCount") val totalCount: Int,
-        @SerializedName("pageableCount") val pageableCount: Int,
-        @SerializedName("isEnd") val isEnd: Boolean,
-        @SerializedName("sameName") val sameName: SameName,
+        @SerializedName("total_count") val totalCount: Int,
+        @SerializedName("pageable_count") val pageableCount: Int,
+        @SerializedName("is_end") val isEnd: Boolean,
+        @SerializedName("same_name") val sameName: SameName,
     ) {
         data class SameName(
             @SerializedName("region") val region: List<String>,
             @SerializedName("keyword") val keyword: String,
-            @SerializedName("selectedRegion") val selectedRegion: String,
+            @SerializedName("selected_region") val selectedRegion: String,
         )
     }
 
     data class Document(
         @SerializedName("id") val id: String,
-        @SerializedName("placeName") val placeName: String,
-        @SerializedName("categoryName") val categoryName: String,
-        @SerializedName("categoryGroupCode") val categoryGroupCode: String,
-        @SerializedName("categoryGroupName") val categoryGroupName: String,
+        @SerializedName("place_name") val placeName: String,
+        @SerializedName("category_name") val categoryName: String,
+        @SerializedName("category_group_code") val categoryGroupCode: String,
+        @SerializedName("category_group_name") val categoryGroupName: String,
         @SerializedName("phone") val phone: String,
-        @SerializedName("addressName") val addressName: String,
-        @SerializedName("roadAddressName") val roadAddressName: String,
+        @SerializedName("address_name") val addressName: String,
+        @SerializedName("road_address_name") val roadAddressName: String,
         @SerializedName("x") val x: String,
         @SerializedName("y") val y: String,
-        @SerializedName("placeUrl") val placeUrl: String,
+        @SerializedName("place_url") val placeUrl: String,
         @SerializedName("distance") val distance: String,
     )
 }
