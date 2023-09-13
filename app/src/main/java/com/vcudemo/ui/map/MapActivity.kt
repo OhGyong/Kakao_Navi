a901b6fe35e757009019620f6142ff049f9c2f7a
O_Gyong <ogyong7143@gmail.com>
2023-09-13 10:47:03 +0900

    주석 및 코드 정리

--- app/src/main/java/com/vcudemo/ui/map/MapActivity.kt
+++ app/src/main/java/com/vcudemo/ui/map/MapActivity.kt
@@ -25,60 +25,37 @@ import dagger.hilt.android.AndroidEntryPoint
 class MapActivity: BaseActivity(), OnMapReadyCallback {
     companion object {
         const val TAG = "MapActivity"
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 
     private lateinit var binding: ActivityMapBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-//    private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
 
+    // 내 위치(출발지) 좌표
     private var myLatitude = 0.0
     private var myLongitude = 0.0
-    private var destinationLatitude = 0.0
-    private var destinationLongitude = 0.0
+
+    // 도착지 좌표
+    private var endLatitude = 0.0
+    private var endLongitude = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate()")
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
-//        locationSource =
-//            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(com.vcudemo.BuildConfig.NAVER_CLIENT_ID)
 
         getMyLocation()
-
-        binding.etSearch.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            intent.putExtra("latitude", myLatitude.toString())
-            intent.putExtra("longitude", myLongitude.toString())
-            getSearchResult.launch(intent)
-        }
-
-        binding.ctDetail.setOnClickListener {
-        }
-
-        binding.btnIntentNavi.setOnClickListener {
-            val intent = Intent(this, NavigationActivity::class.java)
-            intent.putExtra("startLatitude", myLatitude)
-            intent.putExtra("startLongitude", myLongitude)
-            intent.putExtra("destinationLatitude", myLatitude)
-            intent.putExtra("destinationLongitude", destinationLongitude)
-            startActivity(intent)
-            finish()
-        }
-
+        onClickListener()
     }
 
     override fun onMapReady(naverMap: NaverMap) {
         Log.d(TAG, "onMapReady()")
 
         this.naverMap = naverMap
-
-        // 위치 추적 모드 on
-//        naverMap.locationSource = locationSource
-//        naverMap.locationTrackingMode = LocationTrackingMode.NoFollow
 
         // 현재 위치 버튼 on
         naverMap.uiSettings.isLocationButtonEnabled = true
@@ -88,6 +65,10 @@ class MapActivity: BaseActivity(), OnMapReadyCallback {
         naverMap.cameraPosition = cameraPosition
     }
 
+    /**
+     * 현재 위치 계산
+     * todo : 위치 권한 체크 필요
+     */
     @SuppressLint("MissingPermission")
     private fun getMyLocation() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -98,49 +79,64 @@ class MapActivity: BaseActivity(), OnMapReadyCallback {
                     myLatitude = location.latitude
                     myLongitude = location.longitude
 
-                    // todo : 지도 객체를 위치 정보를 얻고 초기화를 할지, 위치 정보를 onMapReady에서 얻을 지
                     binding.mapView.getMapAsync(this)
                 }
             }
+    }
+
+    private fun onClickListener() {
+        binding.etSearch.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            intent.putExtra("latitude", myLatitude.toString())
+            intent.putExtra("longitude", myLongitude.toString())
+            getSearchResult.launch(intent)
+        }
+
+        /**
+         * 맵에 포커스 뺏기 위한 조치
+         */
+        binding.ctDetail.setOnClickListener {
+        }
+
+        binding.btnIntentNavi.setOnClickListener {
+            val intent = Intent(this, NavigationActivity::class.java)
+            intent.putExtra("startLatitude", myLatitude)
+            intent.putExtra("startLongitude", myLongitude)
+            intent.putExtra("destinationLatitude", myLatitude)
+            intent.putExtra("destinationLongitude", endLongitude)
+            startActivity(intent)
+            finish()
+        }
     }
 
     private val getSearchResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if(result.resultCode == RESULT_OK) {
                 if(result.data != null) {
+                    // 목적지 상세 View 노출
                     binding.etSearch.setText(result.data!!.getStringExtra("placeName"))
                     binding.tvPlaceName.text = result.data!!.getStringExtra("placeName")
                     binding.tvAddressName.text = result.data!!.getStringExtra("addressName")
                     binding.tvDistance.text = result.data!!.getStringExtra("distance")
-                    destinationLatitude = result.data!!.getDoubleExtra("latitude", 0.0)
-                    destinationLongitude = result.data!!.getDoubleExtra("longitude", 0.0)
-
                     binding.ctDetail.visibility = View.VISIBLE
+
+                    endLatitude = result.data!!.getDoubleExtra("latitude", 0.0)
+                    endLongitude = result.data!!.getDoubleExtra("longitude", 0.0)
                 }
+
+                // 목적지 마커 설정
                 val marker = Marker()
                 marker.width = 70
                 marker.height = 100
-                marker.position = LatLng(destinationLatitude, destinationLongitude)
+                marker.position = LatLng(endLatitude, endLongitude)
                 marker.map = naverMap
 
-                // 카메라 줌 레벨 및 카메라 이동
-                val cameraPosition = CameraPosition(LatLng(destinationLatitude, destinationLongitude), 13.0)
+                // 카메라 이동
+                val cameraPosition = CameraPosition(LatLng(endLatitude, endLongitude), 13.0)
                 naverMap.cameraPosition = cameraPosition
 
             }else {
                 // todo : 그냥 돌아왔을 때 처리하게 있나?
             }
         }
-
-//    override fun onRequestPermissionsResult(requestCode: Int,
-//                                            permissions: Array<String>,
-//                                            grantResults: IntArray) {
-//        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
-//                grantResults)) {
-//            if (!locationSource.isActivated) { // 권한 거부됨
-//            }
-//            return
-//        }
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//    }
 }
